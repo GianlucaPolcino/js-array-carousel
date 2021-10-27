4a68382d8e4d837a02218de3604d32e1a68e63d0
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-10-27 15:34:43 +0200

    fixed html

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,14 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
 ];
+
+let contatore = 0;
+const up = document.querySelector('.gp-up');
+const down = document.querySelector('.gp-down');
+
+console.log(up);
+console.log(down);
+
+down.addEventListener('click', function(){
+    
+}
